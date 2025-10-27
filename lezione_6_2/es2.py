ab2caf58ab2c6ca83726d33f559b2e44c6269bc5
End3r27 <valerio.virzi1@gmail.com>
2025-10-27 10:16:35 +0100

    es2_6_2

---
+++ lezione_6_2/es2.py
@@ -0,0 +1,13 @@
+seq = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for numeri in seq:
+    print("il quadrato di", numeri, "è", numeri**2)
+
+for n in range(1, 100):
+    print(n)
+    if n==50 :
+        print("il quadrato di", n, "è", n**2)
+
+for n in range(1, 6):
+    print("il cubo di", n, "è", n**3)
+
+
